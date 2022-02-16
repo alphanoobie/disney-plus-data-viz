a417cede91de9176606c810733c4c57c420924b4
alphanoobie <hussaingadi.hg@gmail.com>
2022-02-16 16:38:12 +0530

    added data and plotted two graphs

---
+++ main.py
@@ -0,0 +1,26 @@
+#Import dependencies
+import streamlit as st
+import pandas as pd
+import seaborn
+import plotly.express as px
+
+#Project Title
+st.title('Disney Plus Data Visualization')
+
+#Read the data
+df=pd.read_csv('data.csv')
+
+#Plot top 30 director bar graph
+st.subheader('Top 30 Directors')
+keys = df['director'].value_counts().keys().to_list()
+values = df['director'].value_counts().to_list()
+fig = px.bar(x = keys[:30], y = values[:30], data_frame = df,  
+             labels={'x': 'Directors', 'y': 'no of movies'})
+st.plotly_chart(fig)
+
+#Plot movie and tv show comparison pie
+st.subheader('Movie vs TV Show')
+values = df['type'].value_counts()
+keys = df['type'].unique()
+fig = px.pie(df, values=values, names=keys,color=keys, color_discrete_map={'Movie':'cyan','TV Show':'royal blue'})
+st.plotly_chart(fig)
